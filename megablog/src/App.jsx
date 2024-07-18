808776255393c87772d2559391e8007c8935d683
pranav007082 <148082845+pranav007082@users.noreply.github.com>
2024-07-18 15:14:37 +0000

    proj done

---
+++ megablog/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+  return (
+    <>
+      <h1>Pranav's Blog!!</h1>
+    </>
+  )
+}
+
+export default App
